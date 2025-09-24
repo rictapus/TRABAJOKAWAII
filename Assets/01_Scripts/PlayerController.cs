7dd719752f333857ad9aca1d6deb25ddc9ddfdd9
Rabano <RabanoSerio@gmail.com>
2025-09-23 22:38:56 -0300

    colorMort

--- Assets/01_Scripts/PlayerController.cs
+++ Assets/01_Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //El Ricardo es weko
     }
 
     // Update is called once per frame
