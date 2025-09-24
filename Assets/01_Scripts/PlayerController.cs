1bea519f4e0caba57bb3609a8c2aaf7ec46fec12
rictapus <rickcornejo89@gmail.com>
2025-09-24 17:33:15 -0300

    Movimiento jugador
    puse el movimiento del jugador y la camara 1ra persona

--- Assets/01_Scripts/PlayerController.cs
+++ Assets/01_Scripts/PlayerController.cs
@@ -1,16 +1,77 @@
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
+[DisallowMultipleComponent]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [Header("Camara y movimiento")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float mouseSensitivity = 200f;
+    private float horizontalRotation;
+    private float verticalRotation;
+    private Vector3 PlayerMovement;
+    private Transform camTransform;
+    private float currentSpeed;
+
+    [Header("Salto")]
+    public float jumpForce = 10;
+    private Rigidbody rb;
+
+    [Header("Suelo")]
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+    private void Awake()
     {
-        //El Ricardo es weko
+        camTransform = GetComponentInChildren<Camera>().transform;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
     void Update()
     {
-        
-    }
+        #region Movimiento 
+        currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        float moveX = Input.GetAxis("Horizontal") * currentSpeed;
+        float moveZ = Input.GetAxis("Vertical") * currentSpeed;
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        PlayerMovement = new Vector3(move.x, 0, move.z);
+
+        transform.Translate(PlayerMovement * Time.deltaTime, Space.World);
+        #endregion
+
+        #region Camara
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        horizontalRotation += mouseX;
+        verticalRotation = Mathf.Clamp(verticalRotation - mouseY, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(0, horizontalRotation, 0);
+        camTransform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+        #endregion
+
+        #region Suelo
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        #endregion
+
+        #region Salto
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        #endregion
+        }
 }
